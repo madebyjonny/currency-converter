dede8c061f7c2a24336ed8b6450558b5c77aa4b2
jonathan hamilton <longjonns@gmail.com>
2025-09-10 19:41:17 +0100

    fixed lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from "@eslint/js";
 import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
@@ -11,6 +12,7 @@ export default tseslint.config([
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
+      reactHooks.configs["recommended-latest"],
       reactRefresh.configs.vite,
     ],
     languageOptions: {
